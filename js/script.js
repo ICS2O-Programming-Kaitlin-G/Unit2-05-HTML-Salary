8f604b42bb4f8e13a91c36938ab8eb9daf8323d2
Katie <99366154+kaitlingreen@users.noreply.github.com>
2022-03-31 04:05:58 +0000

    i literally dont know whats wrong with it what is wrong with it why wont it work please why wont you work i just want to know please im just wondering why wont it work please

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Created by: Katie
+// Created on: March 2022
+// This file contains the JS functions for index.html
+
+/**
+ * This function will find the total salary of a citizen and the amount of money the government takes. 
+ */
+function calculateMoney() {
+  // i grab what you have inputted. god how could i. im just so evil.
+  let hourly_wage = parseFloat(document.getElementById('hourly_wage').value);
+  let hours_worked = parseFloat(document.getElementById('hours_worked').value);
+  const tax_rate = 0.1805;
+
+  //now we do Math
+  let whole_earnings = hourly_wage * hours_worked;
+  let government_takes = whole_earnings * tax_rate;
+  let whole_received = whole_earnings - government_takes
+
+  // and now we print it :)
+  document.getElementById('total_earnings').innerHTML = 'With these values, you have made $' + whole-earnings.toFixed(2) + '.'
+  document.getElementById('government_claims').innerHTML = 'With these values, the government is stealing $' + government-takes.toFixed(2) + '.'
+}
